c29b1ff8f4cc2c057ccf9f0e6c0fd415a2bd9db2
Rhizka Subentar <rhizka.sub@upi.edu.com>
2022-03-27 23:10:15 +0700

    Added signup/login, css reform and reworked index

---
+++ js/signup.js
@@ -0,0 +1,23 @@
+function checkPassword() {
+    var pwValue = document.getElementById("password").value;
+    var setMessage = document.getElementById("pwmessage");
+    // Cek kalau kosong
+    if(pwValue == "") {
+       setMessage.innerHTML = "**Fill the password please!";
+       return false;
+    }
+   
+   // Kalau kurang dari 8 char
+    if(pwValue.length < 8) {
+       setMessage.innerHTML = "**Password length must be at least 8 characters";
+       return false;
+    }
+  
+  // Kalau lebih dari 15 char
+    if(pwValue.length > 15) {
+       setMessage.innerHTML = "**Password length must not exceed 15 characters";
+       return false;
+    } else {
+       alert("Password is correct");
+    }
+  }
