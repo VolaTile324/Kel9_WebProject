f09ddf37a49be5356cbbb7f9dc5ee0906359d166
Rhizka Subentar <rhizka.sub@upi.edu.com>
2022-03-29 17:10:30 +0700

    First revision

--- js/search-filter.js
+++ js/search-filter.js
@@ -1,3 +1,27 @@
+var modalIDN = document.getElementById("modal-idn");
+var modalTokopedia = document.getElementById("modal-tokopedia");
+var modalGojek = document.getElementById("modal-gojek");
+
+// Modal IDN Times
+function openModalIDN() {
+    modalIDN.style.display = "block";
+}
+
+function openModalTokopedia() {
+    modalTokopedia.style.display = "block";
+}
+
+function openModalGojek() {
+    modalGojek.style.display = "block";
+}
+
+function closeModal() {
+    modalIDN.style.display = "none";
+    modalTokopedia.style.display = "none";
+    modalGojek.style.display = "none";
+}
+
+// Search Filter
 function showDropdown(){
     var dropdown = document.getElementById("dropdown-list");
     if (dropdown.style.display === "block") {
